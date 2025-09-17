2a6ee151578f65006ecc23b16c824e5b62d63d24
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-09-17 12:08:50 -0300

    feat: obtener el carrito del backend y mostrar cantidad

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -5,7 +5,13 @@ import mobileLogoWhiteImage from "../assets/images/mobile-logo-white.png"
 import searchIcon from "../assets/images/icons/search-icon.png"
 import cartIcon from "../assets/images/icons/cart-icon.png"
 
-export default function Header() {
+export default function Header({cart}) {
+
+    let totalQuantity = 0;
+    cart.forEach((cartItem) => {
+        totalQuantity += cartItem.quantity;
+    })
+
     return (
         <div className="header">
             <div className="left-section">
@@ -32,7 +38,7 @@ export default function Header() {
 
                 <NavLink className="cart-link header-link" to="/checkout">
                     <img className="cart-icon" src={cartIcon} />
-                    <div className="cart-quantity">3</div>
+                    <div className="cart-quantity">{totalQuantity}</div>
                     <div className="cart-text">Cart</div>
                 </NavLink>
             </div>
--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -7,12 +7,18 @@ import checkmarkIcon from "../../assets/images/icons/checkmark.png";
 export default function HomePage() {
 
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         axios.get("http://localhost:3000/api/products")
             .then((response) => {
                 setProducts(response.data)
             });
+
+        axios.get("http://localhost:3000/api/cart-items")
+            .then((response) => {
+                setCart(response.data)
+            })
     }, []);
 
 
@@ -21,7 +27,7 @@ export default function HomePage() {
             <link rel="icon" type="image/svg+xml" href="./home-favicon.png" />
             <title>Ecommerce Project</title>
 
-            <Header />
+            <Header cart={cart} />
 
             <div className="home-page">
                 <div className="products-grid">
